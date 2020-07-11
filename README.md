d54ed4085dcf6701a5161b97b8427e429a5c618b
Taariq Levack <taariql@gmail.com>
2020-07-11 11:46:00 +0200

    cleanup

--- README.md
+++ README.md
@@ -1,14 +1,14 @@
-# Secret Counter 
+# Secret Counter
 
-Simple secret counter built by following the [Secret Network contracts guide] (https://github.com/enigmampc/enigma-blockchain-contracts-guide)
+This is a simple secret contract and a React interface, built by following the [Secret Network contracts guide](https://github.com/enigmampc/enigma-blockchain-contracts-guide)
 
-## Start the network with docker-compose
+### Start the network
 ```bash
 # Also starts nginx to proxy requests to the api server
 docker-compose up
 ```
 
-## Start the rest-api server
+### Start the rest-server so we can query the network
 ```bash
 # in a new terminal
 docker-compose exec secretdev \
@@ -18,17 +18,41 @@ docker-compose exec secretdev \
   --laddr tcp://0.0.0.0:1317
 ```
 
-# start the faucet
+### Start the faucet, to fund burner wallets
 ```bash
-# todo faucet docs
+git clone https://github.com/enigmampc/SecretNetwork/
+cd SecretNetwork/cosmwasm-js
+# check out develop branch for now
+git checkout develop
+# build the JS components
+yarn build
+# start the faucet
+cd packages/faucet
 yarn dev-start
 ```
 
-# Deploy the contract
-# todo copy from guide
+### Fund the faucet holder (if this is different from your test account)
+```bash
+docker-compose exec secretdev \
+  secretcli tx send \
+  a secret1pkptre7fdkl6gfrzlesjjvhxhlc3r4gmenn2dx \
+  100000000000uscrt --from a \
+  --keyring-backend test --yes
+```
 
-# Start the app
+### Deploy the contract
+```bash
+cd client
+node scripts/deploy_counter.js
+```
+
+### Start the app
 ```bash
 cd client
 yarn start:local
 ```
+
+### If it all works so far, you can play with the counter.
+![](images/gui.png)
+
+### todo SecretJS / privacy
--- client/scripts/deploy_counter.js
+++ client/scripts/deploy_counter.js
@@ -1,29 +1,50 @@
 #!/usr/bin/env node
 
 /* eslint-disable @typescript-eslint/camelcase */
-const { SigningCosmWasmClient, Secp256k1Pen } = require("secretjs");
+const { EnigmaUtils, Secp256k1Pen, SigningCosmWasmClient, pubkeyToAddress, encodeSecp256k1Pubkey } = require("secretjs");
 const fs = require("fs");
-const {loadOrCreateMnemonic, connect } = require("./helpers.ts");
 
+const httpUrl = "http://localhost:1317";
+const faucet = {
+  mnemonic:
+    "economy stock theory fatal elder harbor betray wasp final emotion task crumble siren bottom lizard educate guess current outdoor pair theory focus wife stone",
+  address: "secret1cdycaskx8g9gh9zpa5g8ah04ql0lzkrsxmcnfq",
+};
 
+const customFees = {
+  upload: {
+    amount: [{ amount: "25000", denom: "uscrt" }],
+    gas: "2000000",
+  },
+  init: {
+    amount: [{ amount: "0", denom: "uscrt" }],
+    gas: "500000",
+  },
+  exec: {
+    amount: [{ amount: "0", denom: "uscrt" }],
+    gas: "500000",
+  },
+  send: {
+    amount: [{ amount: "2000", denom: "uscrt" }],
+    gas: "80000",
+  },
+}
 async function main() {
+  const signingPen = await Secp256k1Pen.fromMnemonic(faucet.mnemonic);
+    const myWalletAddress = pubkeyToAddress(
+      encodeSecp256k1Pubkey(signingPen.pubkey),
+      "secret"
+    );
+  const txEncryptionSeed = EnigmaUtils.GenerateNewSeed();
+  const client = new SigningCosmWasmClient(
+    httpUrl,
+    myWalletAddress,
+    (signBytes) => signingPen.sign(signBytes),
+    txEncryptionSeed, customFees
+  );
 
-  const enigmaOptions = {
-    httpUrl: "http://localhost:1317",
-    networkId: "enigma-testnet",
-    feeToken: "uscrt",
-    gasPrice: 0.025,
-    bech32prefix: "enigma",
-  }
-
-  const mnemonic = loadOrCreateMnemonic("foo.key");
-  const {address, client} = await connect(mnemonic, enigmaOptions);
-
-  // const pen = await Secp256k1Pen.fromMnemonic(faucet.mnemonic);
-  // const client = new SigningCosmWasmClient(httpUrl, faucet.address, signBytes => pen.sign(signBytes));
-
-  const wasm = fs.readFileSync(__dirname + "../contracts/counter.wasm");
-  const uploadReceipt = client.upload(wasm, {})
+  const wasm = fs.readFileSync(__dirname + "/../../contracts/contract.wasm");
+  const uploadReceipt = await client.upload(wasm, {})
   console.info(`Upload succeeded. Receipt: ${JSON.stringify(uploadReceipt)}`);
 
   const memo = `Counter`;
