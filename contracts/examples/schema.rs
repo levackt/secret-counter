068410bf32b94680ef5f914748aa7245cdaacaff
Taariq Levack <taariql@gmail.com>
2020-07-11 13:10:29 +0200

    unused ci

--- contracts/examples/schema.rs
+++ contracts/examples/schema.rs
@@ -3,8 +3,8 @@ use std::fs::create_dir_all;
 
 use cosmwasm_schema::{export_schema, remove_schemas, schema_for};
 
-use mysecretcounter2::msg::{CountResponse, HandleMsg, InitMsg, QueryMsg};
-use mysecretcounter2::state::State;
+use mysecretcounter::msg::{CountResponse, HandleMsg, InitMsg, QueryMsg};
+use mysecretcounter::state::State;
 
 fn main() {
     let mut out_dir = current_dir().unwrap();
